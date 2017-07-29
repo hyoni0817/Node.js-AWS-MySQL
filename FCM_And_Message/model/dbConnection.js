0a670da3b8b5cf1814d599b907983ab981b3576b
hyoni0817 <dudu1104@naver.com>
2017-07-30 02:44:55 +0900

    내용 수정

--- FCM_And_Message/model/dbConnection.js
+++ FCM_And_Message/model/dbConnection.js
@@ -1,10 +1,10 @@
 const mysql = require('mysql');
 const pool = mysql.createPool({
-    host : 'localhost',//'hostname',
-    port : 3306,//'portnumber',//Erase the small quotation marks and use them.
-    database : 'test',//'databasename',
-    user : 'root',//'username',
-    password : '2587',//'password',
+    host : 'hostname',
+    port : 'portnumber',//Erase the small quotation marks and use them.
+    database : 'databasename',
+    user : 'username',
+    password : 'password',
     multipleStatements: true
    //waitForConnections:false
 });
