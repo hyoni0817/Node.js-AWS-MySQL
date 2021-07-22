69bbb8fd8f322820204d0c70e7e4ef2eba423e08
hyoni0817 <dudu1104@naver.com>
2021-07-22 23:02:38 +0900

    Add: board 테이블에 USER_ID 컬럼 추가

--- Writing/writingQuery.sql
+++ Writing/writingQuery.sql
@@ -1,9 +1,10 @@
 create table board(
 	WRITE_ID int(10) auto_increment,
+    USER_ID varchar(10) not null,
 	USERNAME varchar(10) not null,
     TITLE varchar(45) not null,
     CONTETNTS MEDIUMTEXT not null,
     primary key(WRITE_ID)
-    );
+);
     
     
